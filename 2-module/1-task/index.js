d356d35977add7bbbce8dc39996a1c4543cb2b2c
KseniyaShcherbich <shcherbichkseniya@gmail.com>
2024-08-21 17:36:25 +0300

    add summ salaries func

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let total = 0;
+  let isNull = false;
+  for (let key in salaries){
+    let value = salaries[key];
+    if(typeof value === 'number' && !isNaN(value) && isFinite(value)){
+      total += value;
+      isNull = true;
+    }
+  }
+  return isNull ? total : 0;
 }
